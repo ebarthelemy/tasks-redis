6c3a9f0d1868b3cb478214083339fc174bdf7952
Edouard Barthelemy <edouard@liveanswer.com>
2017-03-09 11:04:02 -0500

    Add & Delete Tasks

--- app.js
+++ app.js
@@ -23,16 +23,55 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// Routes
+/**********/
+/* Routes */
+/**********/
+
+// Get Tasks
 app.get('/', (req, res) => {
   var title = "Task List"
 
-  // Fetch all the items in the list.
+  // LRANGE: Returns the specified elements of the list stored at key.
   client.lrange('tasks', 0, -1, (err, reply) => {
     res.render('index.ejs', {
       title: title,
       tasks: reply
     })
+  })
+})
+
+// Add Task
+app.post('/tasks/add', (req, res) => {
+  let task = req.body.task
+
+  // RPUSH: Insert all the specified values at the tail of the list stored at key.
+  client.rpush('tasks', task, (err, reply) => {
+    if (err) {
+      console.log('err', err)
+    }
+    console.log("Task successfully added.")
+    res.redirect('/')
+  })
+})
+
+// Delete Task
+app.post('/tasks/delete', (req, res) => {
+  let tasksToDelete = req.body.tasks
+
+  // LRANGE: Returns the specified elements of the list stored at key.
+  client.lrange('tasks', 0, -1, (err, tasks) => {
+    tasks.forEach((task) => {
+      if (tasksToDelete.indexOf(task) > -1) {
+        // LREM: Removes the first count occurrences of elements equal to value from the list stored at key.
+        client.lrem('tasks', 0, task, () => {
+          if (err) {
+            console.log('err', err)
+          }
+        })
+      }
+    })
+    console.log("Tasks successfully deleted.")
+    res.redirect('/')
   })
 })
 
