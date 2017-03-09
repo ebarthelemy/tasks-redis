40e7c67d31a92028ef55fbad5baffae47e69eeaa
Edouard BARTHELEMY <edoubart@Edouards-MBP.home>
2017-03-08 23:28:00 -0500

    Add Redis client & fetch all tasks.

--- app.js
+++ app.js
@@ -7,15 +7,33 @@ const redis = require('redis')
 
 var app = express()
 
+// Create Client
+var client = redis.createClient()
+
+client.on('connect', () => {
+  console.log("Redis server connected...")
+})
+
 // Setup
 app.set('views', path.join(__dirname, 'views'))
+app.set('views engine', 'ejs')
+
+app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Welcome')
+  var title = "Task List"
+
+  // Fetch all the items in the list.
+  client.lrange('tasks', 0, -1, (err, reply) => {
+    res.render('index.ejs', {
+      title: title,
+      tasks: reply
+    })
+  })
 })
 
 // Start
